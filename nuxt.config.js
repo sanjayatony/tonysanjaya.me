4e7816c1d83df9a5850ab82f28ca12247580467f
Tony S <sanjayatony@gmail.com>
2020-12-04 23:02:25 +0700

    change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: "tonysanjaya.me",
+    title: "Tony Sanjaya",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/png", href: "/triangle.png" },
       {
         rel: "stylesheet",
         href: "https://use.typekit.net/kph4cvz.css"
