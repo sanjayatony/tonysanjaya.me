21811d510f4def91c05cafa2533494753ef1a389
Tony S <sanjayatony@gmail.com>
2020-12-05 00:37:48 +0700

    update single

--- pages/post/_slug.vue
+++ pages/post/_slug.vue
@@ -3,6 +3,12 @@
     <article class="prose">
       <h1>{{ post.title }}</h1>
       <nuxt-content :document="post" />
+      <div class="text-xs font-mono text-80-cyan mt-10">
+        {{ formatDate(post.date) }} -
+        <span v-for="tag in post.tags" :key="tag">
+          {{ tag }}
+        </span>
+      </div>
     </article>
   </div>
 </template>
@@ -11,6 +17,69 @@ export default {
   async asyncData({ $content, params }) {
     const post = await $content(`post/${params.slug}` || "index").fetch();
     return { post };
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: "numeric", month: "short", day: "numeric" };
+      return new Date(date).toLocaleDateString("en", options);
+    }
+  },
+  head() {
+    return {
+      title: this.post.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.post.excerpt
+        },
+        {
+          hid: "og-type",
+          property: "og:type",
+          content: "website"
+        },
+        {
+          hid: "og-url",
+          property: "og:url",
+          content: "website"
+        },
+        {
+          hid: "og-title",
+          property: "og:title",
+          content: this.post.title
+        },
+        {
+          hid: "og-description",
+          property: "og:description",
+          content: this.post.excerpt
+        },
+        {
+          hid: "og-image",
+          property: "og:image",
+          content: this.post.img
+        },
+        {
+          hid: "tw-card",
+          property: "tw:card",
+          content: "summary_large_image"
+        },
+        {
+          hid: "tw-title",
+          property: "tw:title",
+          content: this.post.title
+        },
+        {
+          hid: "tw-description",
+          property: "tw:description",
+          content: this.post.excerpt
+        },
+        {
+          hid: "tw-image",
+          property: "tw:image",
+          content: this.post.img
+        }
+      ]
+    };
   }
 };
 </script>
