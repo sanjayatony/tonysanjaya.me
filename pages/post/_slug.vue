13048f8a46433b9cc4ef160cd652fe265f46a677
Tony S <sanjayatony@gmail.com>
2020-12-04 23:44:34 +0700

    single post

---
+++ pages/post/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <article class="prose">
+      <h1>{{ post.title }}</h1>
+      <nuxt-content :document="post" />
+    </article>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const post = await $content(`post/${params.slug}` || "index").fetch();
+    return { post };
+  }
+};
+</script>
